b8d36a78bd739b4831e384641e862bd9a5d8903f
BisiDavi <oludavidconnect@gmail.com>
2022-04-08 22:26:06 +0100

    error fixed

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Logo({className}) {
+interface LogoProps {
+  className: string;
+}
+
+export default function Logo({ className }: LogoProps) {
   return (
     <Link href="/" passHref>
       <a title="welcome to live healthy" className={className}>
